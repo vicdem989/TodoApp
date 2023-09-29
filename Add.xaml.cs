f1aca48db620c708393ef1fb93e7e8eccce5fb63
vicdem989 <vicdemhoy@hotmail.com>
2023-09-29 22:26:34 +0200

    Add project files.

---
+++ Add.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Todo_App
+{
+    /// <summary>
+    /// Interaction logic for Add.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        public List<ToDoElement> toDos = new List<ToDoElement>();
+
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtInputName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            ToDoElement task1 = new ToDoElement();
+            addToList(task1, TxtInputName.Text, TxtInputDesc.Text);
+
+            TxtTempOutput.Text = "";
+            foreach (ToDoElement task in toDos)
+            {
+                TxtTempOutput.Text += (task1.name + " " +task1.description);
+            }
+
+        }
+
+        public void addToList(ToDoElement ElementName, string Name, string Description)
+        {
+            ElementName.name = Name;
+            ElementName.description = Description;
+            toDos.Add(ElementName);
+        }
+    }
+
+    public class ToDoElement
+    {
+        public int id = 0;
+        public string name = string.Empty;
+        public string description = string.Empty;
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Todo_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            Add OpenAddWindow = new Add();
+            OpenAddWindow.Show();
+        }
+    }
+}
